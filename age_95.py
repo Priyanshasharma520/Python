1c6cd02db6bb3c02137aac43dd82b257c8eb8ef7
Priyansha Sharma <55023456+Priyanshasharma520@users.noreply.github.com>
2020-01-16 19:45:10 +0530

    Update age_95.py

--- age_95.py
+++ age_95.py
@@ -3,12 +3,22 @@
 
 
 
-
-
+# importing the data and time module
 import datetime
 
+# taking the age as input from the user 
 age=int(input("please enter the age: "))
+
+#extracting the current date and time
 date=datetime.datetime.today()
+print("Date: "date)
+
+#Extracting the curent year
 year=date.year
+
+#sub the age getting the year of birth and then adding  95 to it
 x=str((year-age)+95)
+
+
 print("Year in which you will celebrate your 95th birtday is: "+ x )
+
